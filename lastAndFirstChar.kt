c113c66d53d8266de6726f560f08d00ee670118c
Hirakjyoti Talukdar <76906387+hirakjyoti08@users.noreply.github.com>
2023-03-18 19:00:54 +0530

    Added a file to print last and first chararacter

---
+++ lastAndFirstChar.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val mystr = "HELLO WORLD"
+    var firstCharInStr = mystr[0]
+    var lastCharInStr = mystr[mystr.length - 1]
+    print("the first character is \n" + firstCharInStr)
+    print("\nthe last character is\n" + lastCharInStr)
+}
